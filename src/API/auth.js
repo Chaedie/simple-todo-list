088d0aac6d6ad7859611c988c35eeeccb97574ff
Chaedie <chaedong.im.dev@gmail.com>
2022-10-01 08:56:55 +0900

    Add: 로그인/회원가입 구현
    Style: 공통 스타일 분리
    Add: Auth 기능 구현
    Refactor: lint 리뷰 반영

---
+++ src/API/auth.js
@@ -0,0 +1,17 @@
+// export const API = {
+//   SIGNUP: {
+//     URL: '/auth/signup',
+//     OPTIONS: {
+//       method: 'POST',
+//       headers: {
+//         'content-type': 'application/json',
+//       },
+//       body: {},
+//     },
+//   },
+// };
+
+// export const postSignUp = async () => {
+//   try {
+//   }
+// };
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,22 +1,42 @@
+import { useState } from 'react';
 import './Auth.scss';
+import AuthForm from './AuthForm.js';
 
 function Auth() {
+  const [authType, setAuthType] = useState('login');
+
   return (
-    <div className="Auth">
+    <div className="Auth modal">
       <header>
-        <h1>Auth Page</h1>
+        <h1 className="mg-0_5rem">Auth Page</h1>
       </header>
-      <main className="LoginContainer">
-        <form>
-          <label htmlFor="id">ID</label>
-          <input type="text" name="id"></input>
-          <br></br>
-          <label htmlFor="password">PWD</label>
-          <input type="password" name="password"></input>
-        </form>
+      <main className="authContainer">
+        <h2>{authType}</h2>
+        <input
+          className="mg-0_5rem"
+          type="button"
+          value="Login!"
+          onClick={() => setAuthType('login')}
+        />
+        <input
+          className="mg-0_5rem"
+          type="button"
+          value="signup!"
+          onClick={() => setAuthType('signup')}
+        />
+        <AuthForm authType={authType} />
       </main>
     </div>
   );
 }
 
 export default Auth;
+
+// 요청
+// URL: /auth/signup
+// Method: POST
+// Headers:
+// Content-Type: application/json
+// Body:
+// email: string
+// password: string
