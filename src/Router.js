bd394c894d84dcd4bf2c542b40870c0c77a155bd
Chaedie <chaedong.im.dev@gmail.com>
2022-10-03 07:43:58 +0900

    Add: TodoList Get 구현, Todo Post 구현
    Refactor: API 함수 분리
    함수 분리를 했지만 생각보다 깔끔하지 않다.
    
    Refactor: utils.js의 requst 함수 사용하여 API함수 정리
    Add: Update, Delete 구현
    Refactor: TodoUpdate 컴포넌트 분리
    Fix: 수정 후 위치 변경 되지 않도록 로직 수정, 에러 수정

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Auth from './Auth/Auth';
-import TodoList from './Todo/TodoList';
+import Todo from './Todo/Todo';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />}></Route>
-        <Route path="/todo" element={<TodoList />}></Route>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/api/todo.js
@@ -0,0 +1,58 @@
+import { baseUrl } from '../api';
+import { request } from '../utils';
+
+export const API = {
+  getTodoList: async function (token, bodyData = {}) {
+    const uri = `${baseUrl}/todos`;
+    const options = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+
+    const response = await request(uri, options);
+    return response;
+  },
+
+  postTodo: async (token, bodyData = {}) => {
+    const uri = `${baseUrl}/todos`;
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      // { todo: String, isCompleted: Boolean }
+      body: JSON.stringify(bodyData),
+    };
+
+    const response = await request(uri, options);
+    return response;
+  },
+
+  putTodo: async (token, bodyData = {}) => {
+    const uri = `${baseUrl}/todos/${bodyData?.id}`;
+    const options = {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bodyData),
+    };
+
+    const response = await request(uri, options);
+    return response;
+  },
+
+  deleteTodo: async (token, bodyData = {}) => {
+    const uri = `${baseUrl}/todos/${bodyData?.id}`;
+    const options = {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await request(uri, options);
+    return response;
+  },
+};
