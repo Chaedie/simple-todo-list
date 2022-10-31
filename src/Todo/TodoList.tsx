4ae42263e2b6fab1280c35354bb652d0f8da6105
Chaedie <chaedong.im.dev@gmail.com>
2022-11-01 01:32:06 +0900

    Refactor: TodoList 컴포넌트와 Item 컴포넌트 하나로 병합

--- src/Todo/TodoList.tsx
+++ src/Todo/TodoList.tsx
@@ -1,14 +1,59 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import TodoService from '../api/TodoService';
 import { TodoContext } from '../contexts/TodoContext';
-import Item from './Item';
+import { toggleState } from '../utils/utils';
+import TodoUpdate from './TodoUpdate';
+import './Todo.scss';
 
 function TodoList() {
-  const { todoList } = useContext(TodoContext)!;
+  const { todoList, setTodoList } = useContext(TodoContext);
+  const [isClickedUpdate, setIsClickedUpdate] = useState(false);
+  const [isClickedDelete, setIsClickedDelete] = useState(false);
+
+  const handleDeleteTodo = useCallback(
+    async (id: number) => {
+      await TodoService.delete({ id });
+      const newTodoList = todoList.filter(x => x.id !== id);
+      setTodoList([...newTodoList]);
+      setIsClickedDelete(false);
+    },
+    [todoList, setTodoList]
+  );
 
   return (
     <div className="TodoList">
       {todoList.map(todoItem => {
-        return <Item key={todoItem.id} todoItem={todoItem} />;
+        const { id, todo, isCompleted } = todoItem;
+        return (
+          <div key={id} className="Todo mg-0_5rem">
+            <span>
+              {isCompleted ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+            </span>
+            <span className={isCompleted ? 'completed' : ''}>할일: {todo}</span>
+            <div className="buttons">
+              <button onClick={() => toggleState(setIsClickedUpdate)}>
+                수정
+              </button>
+              <button onClick={() => toggleState(setIsClickedDelete)}>
+                삭제
+              </button>
+            </div>
+            {isClickedUpdate && (
+              <TodoUpdate
+                todoItem={todoItem}
+                setIsClickedUpdate={setIsClickedUpdate}
+              />
+            )}
+            {isClickedDelete && (
+              <div>
+                <button onClick={() => handleDeleteTodo(id)}>
+                  정말로 삭제하시겠어요?
+                </button>
+              </div>
+            )}
+          </div>
+        );
       })}
     </div>
   );
